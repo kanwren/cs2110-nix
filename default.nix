ee2ac252d88b0dcffc71c0633f76bbd2a5d4dacb
nprindle <nprindle18@gmail.com>
2020-01-08 00:32:02 -0500

    Make docker optional, put cs2110docker in env

--- default.nix
+++ default.nix
@@ -3,43 +3,51 @@
 # from his GitHub or from his PPA at
 # http://ppa.launchpad.net/tricksterguy87/ppa-gt-cs2110/ubuntu/
 
-{ pkgs ? let nixpkgs = import ./nixpkgs.nix; in import nixpkgs {}
+{ pkgs         ? let nixpkgs = import ./nixpkgs.nix; in import nixpkgs {}
+, enableDocker ? true
 }:
 
-rec {
-  # The cs2110 docker image itself
-  docker-image = pkgs.callPackage (import ./docker-image.nix) {};
+let
+  inherit (pkgs) lib;
 
-  # Wrapper script to manage the cs2110 docker image
-  # Override enableDockerMachine to true if you're using docker-machine with a
-  # non-localhost default IP
-  cs2110docker = pkgs.callPackage (import ./cs2110docker.nix) {
-    inherit docker-image;
+  dockerPkgs = rec {
+    # The cs2110 docker image itself
+    docker-image = pkgs.callPackage (import ./docker-image.nix) {};
+
+    # Wrapper script to manage the cs2110 docker image
+    # Override enableDockerMachine to true if you're using docker-machine with a
+    # non-localhost default IP
+    cs2110docker = pkgs.callPackage (import ./cs2110docker.nix) {
+      inherit docker-image;
+    };
   };
 
-  complx-tools = pkgs.callPackage (import ./complx-tools.nix) {};
+  cs2110pkgs = rec {
+    complx-tools = pkgs.callPackage (import ./complx-tools.nix) {};
 
-  cs2110-gba-linker-script = pkgs.callPackage (import ./cs2110-gba-linker-script.nix) {};
-  cs2110-vbam-sdl = pkgs.callPackage (import ./cs2110-vbam-sdl.nix) {};
-  nin10kit = pkgs.callPackage (import ./nin10kit.nix) {};
+    cs2110-gba-linker-script = pkgs.callPackage (import ./cs2110-gba-linker-script.nix) {};
+    cs2110-vbam-sdl = pkgs.callPackage (import ./cs2110-vbam-sdl.nix) {};
+    nin10kit = pkgs.callPackage (import ./nin10kit.nix) {};
 
-  makeGBA = pkgs.callPackage (import ./makeGBA.nix) {
-    inherit cs2110-vbam-sdl cs2110-gba-linker-script nin10kit;
+    makeGBA = pkgs.callPackage (import ./makeGBA.nix) {
+      inherit cs2110-vbam-sdl cs2110-gba-linker-script nin10kit;
+    };
   };
 
   # Basic environment for homework hacking
   env = pkgs.mkShell {
-    buildInputs = with pkgs; [
+    buildInputs = (with pkgs; [
       cmake
       gcc
       gdb
       valgrind
-
+    ]) ++ (with cs2110pkgs; [
       complx-tools
       cs2110-vbam-sdl
       nin10kit
-    ];
+    ]) ++ lib.optional enableDocker dockerPkgs.cs2110docker;
   };
 
-}
+in
+  cs2110pkgs // lib.optionalAttrs enableDocker dockerPkgs // { inherit env; }
 
