c7b8a5d5ee15dbf7e091ec8f606e61d33cf0cb78
nprindle <nprindle18@gmail.com>
2020-03-23 18:19:23 -0400

    Add desktop file for nin10kitgui

--- nin10kit.nix
+++ nin10kit.nix
@@ -1,31 +1,39 @@
-{ stdenv, fetchFromGitHub
+{ stdenv, lib, fetchFromGitHub, makeWrapper
+, makeDesktopItem
 , cmake, pkgconfig
 , wxGTK31, imagemagick
+, createDesktop ? true
 }:
 
-stdenv.mkDerivation {
-  name = "nin10kit";
-  nativeBuildInputs = [
-    cmake
-    pkgconfig
-  ];
-  buildInputs = [
-    wxGTK31
-  ];
-  propagatedBuildInputs = [
-    imagemagick
-  ];
-  # http://ppa.launchpad.net/tricksterguy87/ppa-gt-cs2110/ubuntu/pool/main/n/nin10kit/nin10kit_1.7.1.orig.tar.bz2
+let
+  pname = "nin10kit";
+  version = "1.7";
+  desktopItem = makeDesktopItem {
+    name = pname;
+    exec = "nin10kitgui";
+    desktopName = pname;
+    genericName = pname;
+  };
+in stdenv.mkDerivation ({
+  inherit pname version;
+
+  nativeBuildInputs = [ cmake pkgconfig ];
+  buildInputs = [ wxGTK31 imagemagick ];
+
   src = fetchFromGitHub {
     owner = "TricksterGuy";
     repo = "nin10kit";
     rev = "dbf81c62c0fa2f544cfd22b1f7d008a885c2b589";
     sha256 = "1z3kfxqvakx7wkwwcjw1nmp3lg121a56bcw5g1sh9kvmmi2az4fl";
   };
+
   installPhase = ''
     mkdir -p "$out/bin"
     cp nin10kit "$out/bin"
     cp nin10kitgui "$out/bin"
+  '' + lib.optionalString createDesktop ''
+    mkdir -p "$out/share/applications"
+    cp ${desktopItem}/share/applications/* "$out/share/applications"
   '';
-}
+} // lib.optionalAttrs createDesktop { inherit desktopItem; })
 
