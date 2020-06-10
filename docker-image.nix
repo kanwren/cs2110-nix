eb9770d024423c0dbfc3f2a2ca50e83921f3dfc7
nprindle <nprindle18@gmail.com>
2020-06-10 19:57:16 -0400

    Bump docker image to build with complx 4.18.4

--- docker-image.nix
+++ docker-image.nix
@@ -3,9 +3,12 @@
 
 dockerTools.pullImage {
   imageName = "gibrane/cs2110docker";
-  imageDigest = "sha256:e820a8f85fbfca62e9b7e6128e1ddcc2bf709117f51bc94d53b95ad6a72ac594";
-  sha256 = "1dqrdd7m45abhchxr907l487j1sqy2sf3jprbqrh25f534d7apd8";
+  imageDigest = "sha256:5470866eee66ba67f93fa4011476ac0757ed5202b879605003b49f6734e3b42f";
+  sha256 = "0wrqslyddw4dvnfhq4z0add9p54pc58hq7jh6xp1m1hnrsps3bz5";
+
   finalImageName = "gibrane/cs2110docker";
+  finalImageTag = "latest";
+
   os = "linux";
   arch = "amd64";
 }
