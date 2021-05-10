2c3e01983098f624c76870e030f3054a2247943c
Nicole Prindle <nprindle18@gmail.com>
2021-05-09 22:53:02 -0400

    Standardize flake format

--- flake.nix
+++ flake.nix
@@ -6,17 +6,12 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, flake-utils, nixpkgs }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ (import ./mednafen-overlay.nix) ];
-      };
-    in {
-      packages = import ./default.nix { inherit pkgs; };
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages = import ./default.nix { pkgs = nixpkgs.legacyPackages.${system}; };
+    }) // {
       overlay = pkgs-self: pkgs-super: {
         cs2110 = import ./default.nix { pkgs = pkgs-self; };
       };
-    }
-  );
+    };
 }
