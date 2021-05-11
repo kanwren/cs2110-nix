4af997a8f868533bd0407c1b2365ae81eb87d30b
Nicole Prindle <nprindle18@gmail.com>
2021-05-11 02:25:19 -0400

    Use imagemagick7 for nin10kit

--- nin10kit.nix
+++ nin10kit.nix
@@ -1,7 +1,7 @@
 { stdenv, lib, fetchFromGitHub, makeWrapper
 , makeDesktopItem
 , cmake, pkgconfig
-, wxGTK31, imagemagick
+, wxGTK31, imagemagick7
 , createDesktop ? true
 }:
 
@@ -18,14 +18,16 @@ in stdenv.mkDerivation ({
   inherit pname version;
 
   nativeBuildInputs = [ cmake pkgconfig ];
-  buildInputs = [ wxGTK31 imagemagick ];
+  buildInputs = [ wxGTK31 imagemagick7 ];
 
   src = fetchFromGitHub {
     owner = "TricksterGuy";
     repo = "nin10kit";
-    rev = "dbf81c62c0fa2f544cfd22b1f7d008a885c2b589";
-    sha256 = "1z3kfxqvakx7wkwwcjw1nmp3lg121a56bcw5g1sh9kvmmi2az4fl";
+    rev = "104309a680355da166074c54c988f9d17b9aee16";
+    sha256 = "0p0wdv45iqvablhy4wadwy0v45xk740mw26pr3z7ylpqazpj5s9f";
   };
+
+  cmakeFlags = [ "-DENABLE_MAGICK7_SUPPORT=ON" ];
 
   installPhase = ''
     mkdir -p "$out/bin"
